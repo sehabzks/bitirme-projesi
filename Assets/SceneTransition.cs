261b460aa9f3af78d98c069048ea2929daee667e
sehabzks <99829809+sehabzks@users.noreply.github.com>
2025-04-20 18:51:28 +0300

    upload

---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private string transitionTo;
+
+    [SerializeField] private Transform startPoint;
+
+    [SerializeField] private Vector2 exitDirection;
+
+    [SerializeField] private float exitTime;
+
+    private void Start()
+    {
+        if(GameManager.Instance.transitionedFromScene == transitionTo)
+        {
+            PlayerController.Instance.transform.position = startPoint.position;
+
+            StartCoroutine(PlayerController.Instance.WalkIntoNewScene(exitDirection, exitTime));
+        }
+
+        StartCoroutine(UIManager.Instance.sceneFader.Fade(SceneFader.FadeDirection.Out));
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.CompareTag("Player"))
+        {
+            GameManager.Instance.transitionedFromScene = SceneManager.GetActiveScene().name;
+
+            PlayerController.Instance.pState.cutscene = true;
+            PlayerController.Instance.pState.invincible = true;
+
+            StartCoroutine(UIManager.Instance.sceneFader.FadeAndLoadScene(SceneFader.FadeDirection.In, transitionTo));
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public static CameraFollow Instance;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [SerializeField] private float followSpeed = 0.1f;
+
+    [SerializeField] private Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, PlayerController.Instance.transform.position + offset, followSpeed);
+    }
+}
