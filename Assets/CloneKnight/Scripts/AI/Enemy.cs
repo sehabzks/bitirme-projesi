1e9fcb8502dcba31f09cd5db8fa579187177d4df
sehabzks <99829809+sehabzks@users.noreply.github.com>
2025-05-16 01:03:02 +0300

    enemy aı and anim

--- Assets/CloneKnight/Scripts/AI/Enemy.cs
+++ Assets/CloneKnight/Scripts/AI/Enemy.cs
@@ -5,36 +5,34 @@ public class Enemy : MonoBehaviour
     [SerializeField] protected float health;
     [SerializeField] protected float recoilLength;
     [SerializeField] protected float recoilFactor;
-    [SerializeField] protected bool isRecoiling = false; //! Deprecated, timerınkiyle değiştir 
+    [SerializeField] protected bool isRecoiling = false;
 
     [SerializeField] protected float speed;
-
     [SerializeField] protected float damage;
 
     protected Timer recoilTimer = new();
     protected Rigidbody2D rb;
-
-
-
-
+    protected Animator animator;
+    protected bool isDead = false;
 
     protected virtual void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         recoilTimer.duration = recoilLength;
     }
+
     protected virtual void Update()
     {
-        if (IsDead())
+        if (IsDead() && !isDead)
         {
-            Destroy(gameObject);
-            //TODO add death animation, manage with event, make HandleDeath()
+            HandleDeath();
         }
 
         HandleRecoil();
     }
 
-    bool IsDead() => health <= 0;
+   protected bool IsDead() => health <= 0;
 
     void HandleRecoil()
     {
@@ -45,21 +43,40 @@ public class Enemy : MonoBehaviour
         isRecoiling = false;
     }
 
+    protected virtual void HandleDeath()
+    {
+        isDead = true;
+        if (animator != null)
+        {
+            animator.SetTrigger("Death");
+        }
+        Destroy(gameObject, 1.0f); // Ölüm animasyonu için gecikmeli silme
+    }
+
     public virtual void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitForce)
     {
+        if (isDead) return;
+
         health -= _damageDone;
         if (isRecoiling) return;
+
         rb.AddForce(-_hitForce * recoilFactor * _hitDirection);
         isRecoiling = true;
     }
+
     protected void OnCollisionStay2D(Collision2D _other)
     {
         if (!_other.gameObject.CompareTag("Player") || PlayerController.Instance.pState.invincible) return;
         Attack();
         PlayerController.Instance.HitStopTime(0, 5, 0.5f);
     }
+
     protected virtual void Attack()
     {
+        if (animator != null)
+        {
+            animator.SetTrigger("Attack");
+        }
         PlayerController.Instance.TakeDamage(damage);
     }
 }
